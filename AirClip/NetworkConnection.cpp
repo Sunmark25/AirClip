b39177666c9a4b5fdb7a0a71bf266c9c96434976
Binchi Zhang <bzhan484@uwo.ca>
2023-11-28 23:22:40 -0500

    Complete Merge

--- AirClip/NetworkConnection.cpp
+++ AirClip/NetworkConnection.cpp
@@ -9,113 +9,81 @@
 
 #include "NetworkConnection.h"
 
+void NetworkConnection::startServer() {
+    crow::SimpleApp app; // Define the crow application
 
-/**
-    * @brief Constructor for NetworkConnection.
-    *
-    * Initializes a new NetworkConnection instance. Sets up a socket for the server
-    * using the given IP address and port. If socket creation fails, the program will terminate.
-    *
-    * @param ipAddress The IP address of the server.
-    * @param port The port number on which the server will listen for connections.
-    */
-NetworkConnection::NetworkConnection(const std::string &ipAddress, int port)
-        : ipAddress(ipAddress), port(port), addrlen(sizeof(address)) {
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-}
+    CROW_ROUTE(app, "/api/")([]() {
+        return "Welcome to the API";
+    });
 
-/**
-    * @brief Destructor for NetworkConnection.
-    *
-    * Closes the server's file descriptor to properly release the network resources.
-    */
-NetworkConnection::~NetworkConnection() {
-    close(server_fd);
-}
+    // Used to receive clipboard data from the client
+    // TODO: Improve, add authentication so this can't be spoofed
+    // Use: `curl -d '{"username":"<username>","content":"<string-content>"}' 0.0.0.0:18080/api/clipboard/send`
+    CROW_ROUTE(app, "/api/clipboard/send")
+            .methods("POST"_method)([](const crow::request &req) {
+                std::cout << "Raw body: " << req.body << std::endl;
 
-/**
-     * @brief Starts the server.
-     *
-     * Sets socket options, binds the server to the given address and port, and starts listening for incoming connections.
-     * @return bool True if the server starts successfully, false if any step fails.
-     */
-bool NetworkConnection::startServer() {
-    int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        return false;
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
+                auto jsonData = crow::json::load(req.body);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        return false;
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        return false;
-    }
-    runningServer = true;
-    return true;
-}
+                if (!jsonData)
+                    return crow::response(400);
 
-/**
-     * @brief Accepts a new client connection.
-     *
-     * Accepts an incoming client connection request and establishes a connection.
-     * @return bool True if the connection is successfully accepted, false otherwise.
-     */
-bool NetworkConnection::acceptConnection() {
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
-        perror("accept");
-        return false;
-    }
-    return true;
-}
+                std::cout << "Username: " << jsonData["username"].s() << std::endl;
+                std::cout << "Content: " << jsonData["content"].s() << std::endl;
 
-/**
-     * @brief Reads data from the connected client.
-     *
-     * Reads data sent by the client into the provided buffer.
-     * @param buffer The buffer to store the read data.
-     * @param bufferSize The size of the buffer.
-     * @return bool True if data is successfully read, false otherwise.
-     */
-bool NetworkConnection::readData(char* buffer, size_t bufferSize) {
-    ssize_t valread = read(new_socket, buffer, bufferSize - 1);
-    if (valread < 0) {
-        perror("read");
-        return false;
-    }
-    buffer[valread] = '\0';
-    return true;
-}
+                return crow::response{"Received!"};
+            });
 
-/**
-     * @brief Sends data to the connected client.
-     *
-     * Sends the specified data to the client.
-     * @param data The data to be sent.
-     * @return bool True if the data is successfully sent, false otherwise.
-     */
-bool NetworkConnection::sendData(const char* data) {
-    if (send(new_socket, data, strlen(data), 0) < 0) {
-        perror("send");
-        return false;
-    }
-    return true;
-}
+    // TODO: Implement a websocket to listen for clipboard changes from the server
+    // https://stackoverflow.com/a/12442805
+    // https://crowcpp.org/master/guides/websockets/
 
-/**
-     * @brief Closes the connection with the client.
-     *
-     * Closes the client's socket, effectively terminating the connection.
-     */
-void NetworkConnection::closeConnection() {
-    close(new_socket);
+    std::string clipboardContent = "Test data 123";
+
+    // Used to send clipboard data to the client
+    // TODO: Improve, add authentication so this can't be spoofed
+    CROW_ROUTE(app, "/api/clipboard/receive")
+            .methods("GET"_method)([&clipboardContent] {
+                crow::json::wvalue x({"content", clipboardContent});
+                return x;
+            });
+
+    // Used login a user into the server
+    CROW_ROUTE(app, "/api/auth/login")
+            .methods("POST"_method)([](const crow::request &req) {
+                std::cout << "Raw body: " << req.body << std::endl;
+
+                auto jsonData = crow::json::load(req.body);
+
+                if (!jsonData)
+                    return crow::response(400);
+
+                std::cout << "Username: " << jsonData["username"].s() << std::endl;
+                std::cout << "Password: " << jsonData["password"].s() << std::endl;
+
+                // TODO: Login user
+
+                return crow::response{"Logged in!"}; // TODO: Change to something better? UserID? WtConnectionID?
+            });
+
+    // Used register a user in the server
+    CROW_ROUTE(app, "/api/auth/register")
+            .methods("POST"_method)([](const crow::request &req) {
+                std::cout << "Raw body: " << req.body << std::endl;
+
+                auto jsonData = crow::json::load(req.body);
+
+                if (!jsonData)
+                    return crow::response(400);
+
+                std::cout << "Username: " << jsonData["username"].s() << std::endl;
+                std::cout << "Password: " << jsonData["password"].s() << std::endl;
+
+                // TODO: Register user
+
+                return crow::response{"Registered!"}; // TODO: Change to something better? UserID? WtConnectionID?
+            });
+
+    // Set the port, set app to run on multiple threads, and run the app
+    app.port(49162).multithreaded().run(); // .bindaddr("10.0.0.34")
 }
